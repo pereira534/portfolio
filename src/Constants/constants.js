8cd8583a288b094f149039ac79845c8ec0683401
samuel.p <samuel.pereira920427@gmail.com>
2025-05-02 17:57:43 +0300

    update About2

--- src/Constants/constants.js
+++ src/Constants/constants.js
@@ -97,7 +97,7 @@ const introduction = {
   text: [
     "Hello there, I'm so happy you are here! (:",
 
-    "Hello! I'm Samuel, a full stack developer based in Canada. I have a strong passion for the technologies related to QA and AI.",
+    "Hello! I'm Samuel, a full stack developer based in Canada. I have a strong passion for the technologies related to QA and AI API integration including Lanchain, LlamaIndex, Pinecone and Chatgpt(Claude) model.",
     "Ambitious and knowledgeable front-end developer with expertise in web and app development, specializing in clean, efficient code. Proficient in MongoDB, PostgreSQL, Express.js, Nest.js, React.js, React Native, Flutter, Node.js, Next.js, MySQL, HTML5, Tailwind CSS and Typescript, with added experience in blockchain, including Solidity, wallet integration, and NFT marketplaces. Skilled in creating responsive designs for micro-sites, landing pages, and templates, I am driven to exceed expectations and support client success through innovative solutions and troubleshooting. Eager to contribute my technical and interpersonal skills to a dynamic team.",
     "Outside of my regular activities, I'm a football enthusiast, chess player, and a dog lover.",
     "You can download my resume here.",
